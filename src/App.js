e02e5643e0fd3ea159cd77aea46bd097161549fe
ruchik02 <ruchika02>
2021-10-26 19:41:20 +0530

    sloting game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SlotM from "./SlotM";
+const App=()=>{
+  return(
+    <>
+      <h1 className="heading_style"> Welcome to <span style={{fontWeight:'bold'}}>Slot Machine Game</span></h1>
+      <div className="slotmachine">
+      <SlotM x='😁' y='🎅' z='😅'/>
+      <SlotM  x='🙃' y='😆' z='😉'/>
+      <SlotM  x='👍' y='👍' z='👍'/>
+      </div>
+      
+    </>
+  )
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
 
 export default App;
